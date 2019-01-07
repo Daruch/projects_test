fbb61d8266ead3f24db0e545baf7700cd32fb37c
Daruch <dariatalan@gmail.com>
2019-01-07 19:02:51 +0200

    grinder

---
+++ GrinderUdemy/ReactYoutube/ReactStarter/src/index.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import VideoList from "./components/video_list";
+import SearchBar from './components/search_bar';
+import VideoDetail from './components/video_detail';
+let API_KEY = 'AIzaSyCgHT30qY06fRQBRgIxVcMMFbot5oki2sM';
+
+
+class App extends Component {
+   constructor(props){
+       super(props);
+
+       this.state = { 
+            videos: [],
+            selectedVideo:null 
+          };
+
+       this.videoSearch('cats');
+   } 
+
+
+   videoSearch(term){
+    YTSearch({key:API_KEY, term: term}, (videos) => {
+      // console.log(videos);
+       this.setState({
+          videos:videos,
+          selectedVideo: videos[0]
+          });
+        });
+      }
+
+
+   render() {
+     return (
+     <div>
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList 
+          onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
+     </div>
+   );
+ }
+}
+
+ReactDOM.render(<App />, document.querySelector(".container"));
